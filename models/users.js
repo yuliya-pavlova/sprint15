21c684e7956edce0b64943f8c8f8786fedb28c23
Юлия Чечульникова <y.chechulnikova@tinkoff.ru>
2020-08-20 22:34:00 +0300

    + validator for avatar

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -15,6 +16,11 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
+    validate: {
+      validator(link) {
+        return validator.isURL(link);
+      },
+    },
     required: true,
   },
 });
