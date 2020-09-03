9dc6430b0de270e15e98d0db975b88d9434b7f22
Юлия Чечульникова <y.chechulnikova@tinkoff.ru>
2020-09-03 19:24:24 +0300

    + error class

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const users = require('./routes/users.js');
 const { login, createUser } = require('./controllers/users.js');
 const cards = require('./routes/cards.js');
 const auth = require('./middlewares/auth.js');
-const helmet = require('helmet');
 
 const { PORT = 3000 } = process.env;
 
@@ -33,6 +33,19 @@ app.use('/cards', cards);
 app.use((req, res) => {
   res.status('404');
   res.send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
 });
 
 app.listen(PORT, () => {
